0e82497e3f9c51c2228c30fe517f17f581b15051
Soohimi <soohimi2003@gmail.com>
2025-08-11 12:31:47 +0330

    put example for todayhighlight card

--- src/components/highlights/TodayHighlights.jsx
+++ src/components/highlights/TodayHighlights.jsx
@@ -2,9 +2,29 @@ import React from "react";
 
 function TodayHighlights() {
   return (
-    <div>
+    <>
       <div className="pt-6 pl-6 text-xl">Today's Highlight</div>
-    </div>
+      <div className="grid grid-cols-3 gap-4 p-4 justify-between items-center w-full h-full">
+        <div class="bg-gray-800 text-white p-6 rounded-xl text-center">
+          Card 1
+        </div>
+        <div class="bg-gray-800 text-white p-6 rounded-xl text-center">
+          Card 2
+        </div>
+        <div class="bg-gray-800 text-white p-6 rounded-xl text-center">
+          Card 3
+        </div>
+        <div class="bg-gray-800 text-white p-6 rounded-xl text-center">
+          Card 4
+        </div>
+        <div class="bg-gray-800 text-white p-6 rounded-xl text-center">
+          Card 5
+        </div>
+        <div class="bg-gray-800 text-white p-6 rounded-xl text-center">
+          Card 6
+        </div>
+      </div>
+    </>
   );
 }
 
