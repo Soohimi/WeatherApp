09b6a989b39747978d4b069a65464d6f3fb90d0c
Soohimi <soohimi2003@gmail.com>
2025-09-12 11:42:34 +0330

    fixing some issues

--- src/components/layout/MainContainer.jsx
+++ src/components/layout/MainContainer.jsx
@@ -7,10 +7,7 @@ import OtherCountriesList from "../countries/OtherCountriesList";
 
 export default function MainContent() {
   return (
-    // The main container is a simple flex row.
     <main className="flex h-full w-full gap-6">
-      {/* --- LEFT COLUMN --- */}
-      {/* 'flex-1' makes this column take 50% of the available space. */}
       <div className="flex flex-1 flex-col gap-6">
         <section className="rounded-2xl bg-[#1e1e1e] p-4">
           <CurrentWeather />
@@ -20,17 +17,11 @@ export default function MainContent() {
         </section>
       </div>
 
-      {/* --- RIGHT COLUMN --- */}
-      {/* 'flex-1' makes this column take the other 50% of the space. */}
-      {/* 'min-w-0' is the CRITICAL FIX. It forces this column to obey its
-          width and NOT stretch to fit the wide forecast component. */}
       <div className="flex flex-1 flex-col gap-4 min-w-0">
         <section className="flex-1 rounded-2xl bg-[#1e1e1e]">
           <TodayHighlights />
         </section>
 
-        {/* This section's 'overflow-hidden' creates the final boundary
-            that forces the TenDayForecast component inside to scroll. */}
         <section className="flex flex-1 flex-col overflow-hidden rounded-2xl bg-[#1e1e1e] p-4">
           <TenDayForecast />
         </section>
